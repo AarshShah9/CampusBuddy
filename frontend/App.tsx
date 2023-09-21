c24e14c6f41844d84193fb357b5d16e4e5d45750
AarshShah9 <aarshshah99@gmail.com>
2023-09-20 21:23:13 -0600

    Added navigation

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,20 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
 import {Button, StyleSheet, Text, View} from 'react-native';
 
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+function HomeScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Home!</Text>
+        </View>
+    );
+}
+
+function SettingsScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Settings!</Text>
+        </View>
+    );
+}
+
+const Tab = createBottomTabNavigator();
+
 export default function App() {
 
   const testCallback = async () =>{
       console.log("Test")
-    fetch('http://192.168.0.194/Test')
-        .then(response => console.log(response)).catch(error => console.log(error))
+    fetch('https://swapi.dev/api/people/2')
+        .then(response => response.json())
+        .then(data => console.log(data));
   }
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Button title={"Test"} onPress={testCallback} />
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+          <Tab.Navigator>
+              <Tab.Screen name="Home" component={HomeScreen} />
+              <Tab.Screen name="Settings" component={SettingsScreen} />
+          </Tab.Navigator>
+      </NavigationContainer>
+    // <View style={styles.container}>
+    //   <Text>Open up App.tsx to start working on your app!</Text>
+    //   <Button title={"Test"} onPress={testCallback} />
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
 
