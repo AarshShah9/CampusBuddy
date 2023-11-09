4fa1c03bbe53d881a77cbc00bc9573d7fcac603a
AarshShah9 <aarshshah99@gmail.com>
2023-11-08 19:57:53 -0700

    Cleaned

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
-import { ThemeContextProvider } from './contexts/themeContext';
-import { AppContextProvider } from './contexts/appContext';
-import Navigation from './navigation/Navigation';
+import { ThemeContextProvider } from './src/contexts/themeContext';
+import { AppContextProvider } from './src/contexts/appContext';
+import Navigation from './src/navigation/Navigation';
 
 
 export default function App() {
