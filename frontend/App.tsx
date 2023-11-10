bb233906e0f022e25cfec336c58dd96ec87b14c5
JamesRobert20 <jmtendamema@gmail.com>
2023-11-10 09:38:32 -0700

    renamed appcontext to authcontext

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,17 +1,17 @@
 import 'react-native-gesture-handler';
 import { ThemeContextProvider } from './src/contexts/themeContext';
-import { AppContextProvider } from './src/contexts/appContext';
+import { AuthContextProvider } from './src/contexts/authContext';
 import { LoadingContextProvider } from './src/contexts/loadingContext';
 import Navigation from './src/navigation/Navigation';
 
 export default function App() {
     return (
         <ThemeContextProvider>
-            <AppContextProvider>
+            <AuthContextProvider>
                 <LoadingContextProvider>
                     <Navigation />
                 </LoadingContextProvider>
-            </AppContextProvider>
+            </AuthContextProvider>
         </ThemeContextProvider>
     )
 }
---
+++ frontend/src/hooks/useAuthContext.ts
@@ -0,0 +1,11 @@
+import { useContext } from 'react';
+import AuthContext from '../contexts/authContext';
+
+export default function useAuthContext() {
+  const contextValues = useContext(AuthContext);
+
+  if (!contextValues) 
+    throw new Error('useAuthContext must be used within a AuthContextProvider wrapped around the App');
+
+  return contextValues;
+};
