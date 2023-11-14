f26e8ac1de108d0b65d3c6e829e5e25cd074a5b4
AarshShah9 <aarshshah99@gmail.com>
2023-11-14 12:49:09 -0600

    Intentionally pushing with an error

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,7 @@
 const os = require('os');
 import dotenv from 'dotenv';
 
-import express, { Request, Response, NextFunction } from 'express';
+import express, {Request, Response} from 'express';
 
 const result = dotenv.config();
 const ip = process.env.IP_ADDRESS ?? 'localhost';
@@ -10,7 +10,7 @@ const ip = process.env.IP_ADDRESS ?? 'localhost';
 const app = express();
 const port = 3000;
 
-
+d
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
