827d07d52c99a2b07565e661f2c8ca813dff9f32
asallh <ajaypalsallh@gmail.com>
2024-01-24 11:36:56 -0700

    Location chip onto event card
    Signed-off-by: asallh <ajaypalsallh@gmail.com>

--- frontend/src/components/EventHomeCard.tsx
+++ frontend/src/components/EventHomeCard.tsx
@@ -1,14 +1,16 @@
 import * as React from 'react';
 import {ThemedText} from '~/components/ThemedComponents';
 import {Card, Text } from 'react-native-paper';
+import LocationChip from './LocationChip';
 
 export default function EventHomeCard() {  
     return (
-<Card style={{marginLeft: 16, marginRight:16,width:159, height:130}}>
+<Card style={{marginLeft: 16, marginRight:16,width:159, height:130}}> 
     <Card.Cover source={{ uri: 'https://picsum.photos/700' }} style={{width: 159, height:84}}/>
     <Card.Content>
       <ThemedText>Card title</ThemedText>
       <ThemedText>Card content</ThemedText>
+      <LocationChip></LocationChip>
     </Card.Content>
   </Card>
     );
---
+++ frontend/src/components/LocationChip.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { Chip } from 'react-native-paper';
+
+export default function LocationChip(){
+  return (
+    <Chip icon="information" style={{borderRadius:29, height:18, width:'auto'}}>Location Chip</Chip>
+  );
+}
+ 
