01bc5ce10f8def8c238e03ed8cb7f27f7faa2c60
AarshShah9 <aarshshah99@gmail.com>
2024-01-27 20:32:55 -0700

    Added hostname

--- backend/server.ts
+++ backend/server.ts
@@ -86,7 +86,7 @@ app.post(
 app.use(errorHandler);
 
 // server start
-const server = app.listen(8080, () => {
+const server = app.listen(8080, "127.0.0.1", () => {
   console.log(`App listening at http://localhost:${port} !`);
 });
 
