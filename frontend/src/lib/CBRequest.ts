ed32265538cd8b56ed4c0af60474f78d4386bde2
AarshShah9 <aarshshah99@gmail.com>
2024-01-28 15:06:10 -0700

    Refactored

--- frontend/src/lib/CBRequest.ts
+++ frontend/src/lib/CBRequest.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { BACKEND_URL, ENV } from "@env";
+import { ImagePickerAsset } from "expo-image-picker";
+import { Platform } from "react-native";
 
 // Define the array of allowed endpoints
 const allowedEndpoints = ["/Test"] as const;
@@ -43,4 +45,43 @@ const getRequest = async (endpoint: AllowedEndpoints, options: RequestArgs) => {
   return response.data;
 };
 
-export { postRequest, getRequest };
+const UploadImageRequest = async (
+  selectedImage: ImagePickerAsset,
+  data: Record<string, any>,
+) => {
+  const uri =
+    Platform.OS === "android"
+      ? selectedImage.uri
+      : selectedImage.uri.replace("file://", "");
+  const filename = selectedImage.uri.split("/").pop();
+  const match = /\.(\w+)$/.exec(filename as string);
+  const ext = match?.[1];
+  const type = match ? `image/${match[1]}` : `image`;
+  const formData = new FormData();
+  formData.append("file", {
+    uri,
+    name: `image.${ext}`,
+    type,
+  } as any);
+
+  formData.append("data", JSON.stringify(data));
+
+  await axios
+    .post(
+      `https://cuddly-ladybug-innocent.ngrok-free.app/api/events/organization/1`,
+      formData,
+      {
+        headers: { "Content-Type": "multipart/form-data" },
+      },
+    )
+    .then((response) => {
+      console.log(response);
+      alert("Image Uploaded Successfully!");
+    })
+    .catch((err) => {
+      console.log(err);
+      alert("Something went wrong in the Upload Function!");
+    });
+};
+
+export { postRequest, getRequest, UploadImageRequest };
--- frontend/src/screens/Home.tsx
+++ frontend/src/screens/Home.tsx
@@ -7,9 +7,10 @@ import { Button, Platform, StyleSheet, View } from "react-native";
 import { Card } from "react-native-paper";
 import { ThemedText } from "~/components/ThemedComponents";
 import useLoadingContext from "~/hooks/useLoadingContext";
-import { getRequest } from "~/lib/CBRequest";
+import { getRequest, UploadImageRequest } from "~/lib/CBRequest";
 import { EventCreateSchema } from "../../../shared/zodSchemas";
 import { z } from "zod";
+import imageGetter from "~/lib/imageGetter";
 
 export default function Home() {
   const { startLoading, stopLoading } = useLoadingContext();
@@ -27,71 +28,23 @@ export default function Home() {
   const { navigate } = useNavigation<any>();
 
   const pickImage = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
+    const result = await imageGetter();
 
-    if (!result.canceled) {
-      setImage(result.assets[0].uri);
-
-      type EventCreateType = z.infer<typeof EventCreateSchema>;
-
-      const data: EventCreateType = {
-        title: "Test Event",
-        description: "Test Description",
-        location: "Test Location",
-        startTime: new Date(),
-        endTime: new Date(),
-        isPublic: true,
-      };
-
-      await UploadImage(result.assets[0], data);
-
+    if (result.canceled) {
       return;
     }
-    console.log(result);
-  };
 
-  const UploadImage = async (
-    selectedImage: ImagePickerAsset,
-    data: Record<string, any>,
-  ) => {
-    const uri =
-      Platform.OS === "android"
-        ? selectedImage.uri
-        : selectedImage.uri.replace("file://", "");
-    const filename = selectedImage.uri.split("/").pop();
-    const match = /\.(\w+)$/.exec(filename as string);
-    const ext = match?.[1];
-    const type = match ? `image/${match[1]}` : `image`;
-    const formData = new FormData();
-    formData.append("file", {
-      uri,
-      name: `image.${ext}`,
-      type,
-    } as any);
+    type EventCreateType = z.infer<typeof EventCreateSchema>;
 
-    formData.append("data", JSON.stringify(data));
-
-    await axios
-      .post(
-        `https://cuddly-ladybug-innocent.ngrok-free.app/api/events/organization/1`,
-        formData,
-        {
-          headers: { "Content-Type": "multipart/form-data" },
-        },
-      )
-      .then((response) => {
-        console.log(response);
-        alert("Image Uploaded Successfully!");
-      })
-      .catch((err) => {
-        console.log(err);
-        alert("Something went wrong in the Upload Function!");
-      });
+    const data: EventCreateType = {
+      title: "Test Event",
+      description: "Test Description",
+      location: "Test Location",
+      startTime: new Date(),
+      endTime: new Date(),
+      isPublic: true,
+    };
+    await UploadImageRequest(result.assets[0], data);
   };
 
   return (
