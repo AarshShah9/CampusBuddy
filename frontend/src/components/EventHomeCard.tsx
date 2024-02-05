15800599a1457febfc20973a5d94e32b11d6a059
asallh <ajaypalsallh@gmail.com>
2024-02-05 16:33:10 -0700

    home event card appearence, file name change
    Signed-off-by: asallh <ajaypalsallh@gmail.com>

--- frontend/src/components/EventHomeCard.tsx
+++ frontend/src/components/EventHomeCard.tsx
@@ -1,39 +1,46 @@
 import * as React from 'react';
-import {ThemedText} from '~/components/ThemedComponents';
-import {Card} from 'react-native-paper';
+import { ThemedText } from '~/components/ThemedComponents';
+import { Card } from 'react-native-paper';
 import LocationChip from './LocationChip';
-import styled from "styled-components"
+import styled from 'styled-components';
 import { View } from 'react-native';
 
 type EventHomeCardProps = {
   eventData: { title: string; time: string; location: string; image: string };
 };
 
-export default function EventHomeCard({eventData}: EventHomeCardProps) {  
-    return (
-      <CardContainer>
-        <Card.Cover source={{ uri: 'https://picsum.photos/700' }} style={{width: 159, height:84}}/>
-        <Card.Content>
-          <ThemedText>{eventData.title}</ThemedText>
-          <EventDetailsContainer>
-            <ThemedText>{eventData.time}</ThemedText>
-            <LocationChip location = {eventData.location}></LocationChip>
-          </EventDetailsContainer>
-        </Card.Content>
-      </CardContainer>
-    );
-  };
+export default function EventHomeCard({ eventData }: EventHomeCardProps) {
+  return (
+    <StyledCard>
+      <StyledCover source={{ uri: 'https://picsum.photos/700' }} style={{ width: 159, height: 84 }} />
+      <CardContent>
+        <ThemedText>{eventData.title}</ThemedText>
+        <EventDetailsContainer>
+          <ThemedText>{eventData.time}</ThemedText>
+          <LocationChip location={eventData.location} />
+        </EventDetailsContainer>
+      </CardContent>
+    </StyledCard>
+  );
+}
 
-  const CardContainer = styled(Card)`
+const StyledCard = styled(Card)`
   margin-left: 16px;
   margin-right: 16px;
   width: 159px;
   height: 130px;
+  background-color: rgba(0, 0, 0, 0);
 `;
 
+const StyledCover = styled(Card.Cover)`
+  width: 159px;
+  height: 178px;
+`;
 
-  const EventDetailsContainer = styled(View)`
-    flexDirection:'row';
-    align-items:'center';
-  `;
+const CardContent = styled(Card.Content)`
+`;
 
+const EventDetailsContainer = styled(View)`
+  flex-direction:row;
+  justifyContent: "space-between";
+`;
