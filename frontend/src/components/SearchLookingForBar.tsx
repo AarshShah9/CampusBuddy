ec27362ef0e038fd27c06a3390c325c05491807b
asallh <ajaypalsallh@gmail.com>
2024-02-06 19:45:11 -0700

    looking for result component takes props
    Signed-off-by: asallh <ajaypalsallh@gmail.com>

--- frontend/src/components/SearchLookingForBar.tsx
+++ frontend/src/components/SearchLookingForBar.tsx
@@ -1,30 +1,46 @@
 import * as React from "react";
-import { List } from "react-native-paper";
+import { List, Text } from "react-native-paper";
 import { Dimensions, View, useWindowDimensions } from "react-native";
 import styled from "styled-components";
 import PersonChip from "./PersonChip";
 import CommentsChip from "./CommentsChip";
+import { useFonts } from "expo-font";
 
-export default function LookingForItem() {
+type LookingForProps ={
+  lookingForData:{
+    title: string;
+    description: string;
+    numberOfPeople: number;
+  }
+}
+
+export default function LookingForItem({lookingForData}:LookingForProps) {
   const window = useWindowDimensions();
+  const [fontsLoaded] = useFonts({
+    "Nunito-Bold": require("frontend/assets/fonts/Nunito-Bold.ttf"),
+    "Nunito-Reg": require("frontend/assets/fonts/Nunito-Reg.ttf"),
+    "Roboto-Reg": require("frontend/assets/fonts/Roboto-Reg.ttf"),
+    "Roboto-Bold": require("frontend/assets/fonts/Roboto-Bold.ttf"),
+  });
+  
   return (
-    <View
-      style={{
-        height: 174,
-        marginLeft: 16,
-        marginRight: 16,
-        marginBottom: 16,
-        width: window.width,
-        borderBottomWidth: 1,
-        borderBottomColor: "rgb(147, 142, 148)",
-      }}
-    >
-      <PostBar
-        title="Title of the Post"
-        description="lorem ipsum dolor sit amet, consectetur adip, lorem ipsum dolor sit amet"
-      />
-
-      <View style={{ flexDirection: "row", justifyContent: "space-between", marginTop:24 }}>
+    <View style={{borderBottomWidth:0.5, borderBottomColor:"rgb(204, 204, 204)", height:174, width:window.width}}>
+      <InfoContainer>
+        <TitleText>
+          {lookingForData.title}
+        </TitleText>
+        <DescriptionText>
+          {lookingForData.description}
+        </DescriptionText>
+      </InfoContainer>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          marginTop: 'auto',
+          marginBottom: 16
+        }}
+      >
         <PersonChip></PersonChip>
         <CommentsChip></CommentsChip>
       </View>
@@ -32,8 +48,21 @@ export default function LookingForItem() {
   );
 }
 
-const PostBar = styled(List.Item)`
-  paddingvertical: 16px;
+const InfoContainer = styled(View)`
+  margin-top:16px;
+  margin-right:16px;
+  margin-left:16px;
 `;
 
-const PostBarTitle = styled(List.Item)``;
+const TitleText = styled(Text)`
+  font-family: Nunito-Bold;
+  font-size:16px;
+  margin-bottom:8px;
+`;
+
+const DescriptionText = styled(Text)`
+  font-family: Roboto-Reg;
+  font-size:12px;
+  height: 64px;
+  width:361px;
+`;
