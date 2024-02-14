96f8ae313b4ae2611ac98ee558bed041c7c2e739
jcharlpretorius <pretorius.jeancharl@gmail.com>
2024-02-14 00:50:41 -0700

    add auth middleware to all org routes
    Signed-off-by: jcharlpretorius <pretorius.jeancharl@gmail.com>

--- backend/routes/org.routes.ts
+++ backend/routes/org.routes.ts
@@ -13,18 +13,18 @@ import {
   manageNewOrganizationRequest,
 } from "../controllers/org.controller";
 import { upload } from "../utils/S3Uploader";
+import { verifyAuthentication } from "../middleware/verifyAuth";
 
 const router = express.Router();
 
 router.get("/test", organizationTest);
+router.use(verifyAuthentication); // Use auth middleware for all routes below
 router.get("/", getAllOrganizations);
 router.get("/institution/:id", getAllOrganizationsByInstitution);
 router.get("/:id/pendingUsers", getAllPendingOrgUsers);
 router.get("/pending/", getAllPendingOrganizations); // for admin interface
 router.post("/:id/orgApproval", manageNewOrganizationRequest); // for admin interface
-
-router.post("/:id/membership/approval", manageMembershipRequest); // for moderators
-
+router.post("/:id/membership/approval", manageMembershipRequest);
 router.post("/", upload.single("file"), createNewOrganization);
 router.get("/:id", getOrganizationById);
 router.patch("/:id", upload.single("file"), updateOrganization);
