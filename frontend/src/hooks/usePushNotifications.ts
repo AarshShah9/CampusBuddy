13b1b7749671e212aa3b18ded105ddba798766f7
AarshShah9 <aarshshah99@gmail.com>
2024-02-15 12:10:19 -0700

    Added app developer stuff

--- frontend/src/hooks/usePushNotifications.ts
+++ frontend/src/hooks/usePushNotifications.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import * as Device from "expo-device";
 import * as Notifications from "expo-notifications";
 
@@ -83,7 +83,7 @@ export const usePushNotifications = (): PushNotificationState => {
 
     return () => {
       Notifications.removeNotificationSubscription(
-        notificationListener.current!
+        notificationListener.current!,
       );
 
       Notifications.removeNotificationSubscription(responseListener.current!);
