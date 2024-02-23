6e6b9dc74b13d3192fca5c2f1c0bf41d31806f87
JamesRobert20 <jmtendamema@gmail.com>
2024-02-23 14:41:04 -0700

    Completed marketplace page
    Signed-off-by: JamesRobert20 <jmtendamema@gmail.com>

--- frontend/src/components/LocationChip.tsx
+++ frontend/src/components/LocationChip.tsx
@@ -1,20 +1,41 @@
 import { Text } from "react-native-paper";
 import { EvilIcons } from "@expo/vector-icons";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, ViewStyle, StyleProp, TextStyle } from "react-native";
 import useThemeContext from "~/hooks/useThemeContext";
 
-type LocationChipProps = {
+type Props = {
     location: string;
+    size: 'small' | 'normal'
 };
 
-export default function LocationChip({ location }: Readonly<LocationChipProps>) {
+export default function LocationChip({ location, size }: Props) {
     const { theme } = useThemeContext();
 
+    const containerStyles: StyleProp<ViewStyle> = size === 'small' ?
+    {
+        borderRadius: 24,
+        paddingVertical: 2,
+        paddingRight: 4,
+    } 
+    :{
+        borderRadius: 29,
+        paddingVertical: 2.5,
+        paddingRight: 6,
+    }
+
+    const iconSize = size === 'small' ? 15 : 20;
+
+    const textStyles: StyleProp<TextStyle> = size === 'small' ? { fontSize: 10 } : {};
+
     return (
-        <View style={[styles.container, { backgroundColor: theme.colors.inversePrimary }]}>
+        <View 
+            style={[styles.container, containerStyles, { 
+                backgroundColor: theme.colors.inversePrimary 
+            }]}
+        >
             <View style={styles.innerContainer}>
-                <EvilIcons name="location" size={20} color="black" />
-                <Text>{location}</Text>
+                <EvilIcons name="location" size={iconSize} color="black" />
+                <Text style={[styles.text, textStyles]}>{location}</Text>
             </View>
         </View>
     );
@@ -23,16 +44,16 @@ export default function LocationChip({ location }: Readonly<LocationChipProps>) 
 // prettier-ignore
 const styles = StyleSheet.create({
     container: {
-        borderRadius: 29,
-        paddingVertical: 2.5,
-        paddingLeft: 1,
-        paddingRight: 6,
         flexDirection: 'row',
         alignSelf: 'flex-start',
-        justifyContent: 'flex-start'
+        justifyContent: 'flex-start',
+        paddingLeft: 1,
     },
     innerContainer: {
         flexDirection: 'row',
         alignItems: 'center'
+    },
+    text: {
+        fontFamily: 'Nunito-Bold'
     }
 })
