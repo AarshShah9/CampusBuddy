66cf16bf824da56e478d93fc1400d616c540e7dd
AarshShah9 <aarshshah99@gmail.com>
2024-03-03 18:26:09 -0700

    Removed package json changes

--- backend/index.ts
+++ backend/index.ts
@@ -3,7 +3,6 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import express, { NextFunction, Request, Response } from "express";
-import multer from "multer";
 import path from "path";
 import { errorHandler } from "./middleware/errorHandler";
 import event from "./routes/event.routes";
