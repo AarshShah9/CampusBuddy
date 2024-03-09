ebf6e66f46d8bcfcec0cf110d48b3b0c2cc6a078
nomanSanjari <nomansanjari2001@gmail.com>
2024-03-09 00:56:28 -0700

    Recommendation controllers and endpoints added for Events, Users, Posts -> made a ticket for mandatory refactors for post MVP but this will work for now
    Signed-off-by: nomanSanjari <nomansanjari2001@gmail.com>

--- backend/controllers/rec.controller.ts
+++ backend/controllers/rec.controller.ts
@@ -3,7 +3,6 @@ import { RequestExtended } from "../middleware/verifyAuth";
 import prisma from "../prisma/client";
 import cosineSimilarity from "../utils/cosineSimilarity";
 import normalizeTags from "../utils/normalizeTags";
-import { consoleLog } from "@ngrok/ngrok";
 
 export const recommendEvents = async (
   req: RequestExtended,
@@ -43,11 +42,105 @@ export const recommendEvents = async (
     const recommendationMap = new Map();
 
     for (let [key, value] of eventTagMap) {
-      console.log(key);
-      console.log(value);
-      console.log(userTagsNormalized);
       let similarity = cosineSimilarity(userTagsNormalized, value);
-      console.log(similarity);
+      recommendationMap.set(key, similarity);
+    }
+
+    const similarityArray = Array.from(recommendationMap.entries());
+    similarityArray.sort((a, b) => b[1] - a[1]);
+
+    res.status(200).json({ similarityArray });
+  }
+};
+
+export const recommendPosts = async (
+  req: RequestExtended,
+  res: Response,
+  next: NextFunction,
+) => {
+  // modify during integration
+  const loggedInUserId = req.body.userId;
+  const user = await prisma.user.findUnique({
+    where: {
+      id: loggedInUserId,
+    },
+    include: {
+      tags: true,
+    },
+  });
+  const userTags = user?.tags;
+
+  const allTags = await prisma.tag.findMany({});
+
+  if (userTags) {
+    const userTagsNormalized = normalizeTags(userTags, allTags);
+
+    const allPosts = await prisma.post.findMany({
+      include: {
+        tags: true,
+      },
+    });
+
+    const postTagMap = new Map();
+
+    allPosts.forEach((post) => {
+      const eventTagsNormalized = normalizeTags(post.tags, allTags);
+      postTagMap.set(post.id, eventTagsNormalized);
+    });
+
+    const recommendationMap = new Map();
+
+    for (let [key, value] of postTagMap) {
+      let similarity = cosineSimilarity(userTagsNormalized, value);
+      recommendationMap.set(key, similarity);
+    }
+
+    const similarityArray = Array.from(recommendationMap.entries());
+    similarityArray.sort((a, b) => b[1] - a[1]);
+
+    res.status(200).json({ similarityArray });
+  }
+};
+
+export const recommendUsers = async (
+  req: RequestExtended,
+  res: Response,
+  next: NextFunction,
+) => {
+  // modify during integration
+  const loggedInUserId = req.body.userId;
+  const user = await prisma.user.findUnique({
+    where: {
+      id: loggedInUserId,
+    },
+    include: {
+      tags: true,
+    },
+  });
+  const userTags = user?.tags;
+
+  const allTags = await prisma.tag.findMany({});
+
+  if (userTags) {
+    const userTagsNormalized = normalizeTags(userTags, allTags);
+
+    const allUsers = await prisma.user.findMany({
+      include: {
+        tags: true,
+      },
+    });
+
+    const userTagMap = new Map();
+
+    allUsers.forEach((user) => {
+      const eventTagsNormalized = normalizeTags(user.tags, allTags);
+      userTagMap.set(user.id, eventTagsNormalized);
+    });
+
+    const recommendationMap = new Map();
+
+    for (let [key, value] of userTagMap) {
+      let similarity = cosineSimilarity(userTagsNormalized, value);
       recommendationMap.set(key, similarity);
     }
 
--- backend/routes/rec.routes.ts
+++ backend/routes/rec.routes.ts
@@ -1,8 +1,14 @@
 import express from "express";
-import { recommendEvents } from "../controllers/rec.controller";
+import {
+  recommendEvents,
+  recommendPosts,
+  recommendUsers,
+} from "../controllers/rec.controller";
 
 const router = express.Router();
 
 router.get("/recommendEvents", recommendEvents);
+router.get("/recommendPosts", recommendPosts);
+router.get("/recommendUsers", recommendUsers);
 
 export default router;
