37b0e93c208b667c990a43785dad1208f8d77118
AarshShah9 <aarshshah99@gmail.com>
2024-03-10 15:54:04 -0600

    Added call from the frontend

--- frontend/src/components/VerticalScrollView.tsx
+++ frontend/src/components/VerticalScrollView.tsx
@@ -2,43 +2,52 @@ import { FlatList, View, Image, Dimensions } from "react-native";
 import { sampleEventData, carouselImages } from "~/mockData/EventData";
 import HorizontalScrollElement from "~/components/HorizontalScrollElement";
 import Carousel from "pinar";
+import { useEffect } from "react";
+import useLoadingContext from "~/hooks/useLoadingContext";
+import useEventsContext from "~/hooks/useEventsContext";
 
 export default function VerticalScrollComponent() {
-    const screenWidth = Dimensions.get("window").width;
+  const screenWidth = Dimensions.get("window").width;
+  const { startLoading, stopLoading } = useLoadingContext();
+  const { getMainEvents } = useEventsContext();
 
-    return (
-        <FlatList
-            data={sampleEventData}
-            renderItem={HorizontalScrollElement}
-            keyExtractor={(item) => item.id}
-            ListHeaderComponent={
-                <View
-                    style={{
-                        justifyContent: "center",
-                        marginBottom: 32,
-                        alignItems: "center",
-                    }}
-                >
-                    <Carousel
-                        loop={true}
-                        autoplay={true}
-                        autoplayInterval={5000}
-                        showsControls={false}
-                        style={{
-                            width: screenWidth, 
-                            height: 214
-                        }}
-                    >
-                        {carouselImages.map(image => (
-                            <Image
-                                key={image}
-                                source={{ uri: image }}
-                                style={{ width: screenWidth, height: 214 }}
-                            />
-                        ))}
-                    </Carousel>
-                </View>
-            }
-        />
-    );
+  useEffect(() => {
+    const events = getMainEvents();
+  }, []);
+
+  return (
+    <FlatList
+      data={sampleEventData}
+      renderItem={HorizontalScrollElement}
+      keyExtractor={(item) => item.id}
+      ListHeaderComponent={
+        <View
+          style={{
+            justifyContent: "center",
+            marginBottom: 32,
+            alignItems: "center",
+          }}
+        >
+          <Carousel
+            loop={true}
+            autoplay={true}
+            autoplayInterval={5000}
+            showsControls={false}
+            style={{
+              width: screenWidth,
+              height: 214,
+            }}
+          >
+            {carouselImages.map((image) => (
+              <Image
+                key={image}
+                source={{ uri: image }}
+                style={{ width: screenWidth, height: 214 }}
+              />
+            ))}
+          </Carousel>
+        </View>
+      }
+    />
+  );
 }
--- frontend/src/contexts/eventsContext.tsx
+++ frontend/src/contexts/eventsContext.tsx
@@ -1,34 +1,77 @@
 import { useQuery } from "@tanstack/react-query";
-import type { PropsWithChildren } from "react";
-import { createContext } from "react";
+import { createContext, PropsWithChildren, useCallback } from "react";
 import { mockSearchEvents } from "~/mockData/EventData";
+import { CBRequest } from "~/lib/CBRequest";
 
 type Event = {
-    id: string,
-    name: string
-    date: string,
-    location: string,
-    clubName: string,
-    picture: string
-}
+  id: string;
+  name: string;
+  date: string;
+  location: string;
+  clubName: string;
+  picture: string;
+};
+
+type EventResponse = {
+  id: string;
+  userId: string;
+  organizationId: string;
+  createdAt: string;
+  title: string;
+  description: string;
+  startTime: string;
+  endTime: string;
+  isPublic: boolean;
+  status: string;
+  image: string;
+  locationPlaceId: string;
+};
+
+type institutionResponse = {
+  id: string;
+  organizationName: string;
+  description: string;
+  createdAt: string;
+  updatedAt: string;
+  status: string;
+  image: string;
+  institutionId: string;
+};
+
+type MainEventPageResponse = {
+  attendingEvents: EventResponse[];
+  trendingEvents: EventResponse[];
+  verifiedOrganizations: institutionResponse[];
+};
 
 type eventsContext = {
-    events: Event[]
+  events: Event[];
+  getMainEvents: () => Promise<any>;
 };
 const EventsContext = createContext<eventsContext | null>(null);
 
-export const EventsContextProvider = ({ children }: PropsWithChildren): JSX.Element => {
-    const { data: events } = useQuery({
-        queryKey: ['events'],
-        queryFn: async () => mockSearchEvents,
-        initialData: []
-    })
+export const EventsContextProvider = ({
+  children,
+}: PropsWithChildren): JSX.Element => {
+  const { data: events } = useQuery({
+    queryKey: ["events"],
+    queryFn: async () => mockSearchEvents,
+    initialData: [],
+  });
 
-    return (
-        <EventsContext.Provider value={{ events }}>
-            {children}
-        </EventsContext.Provider>
-    );
+  const getMainEvents = useCallback(async () => {
+    try {
+      return await CBRequest("GET", "/api/events/mainPage");
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
+  return (
+    <EventsContext.Provider value={{ events, getMainEvents }}>
+      {children}
+    </EventsContext.Provider>
+  );
 };
 
 export default EventsContext;
