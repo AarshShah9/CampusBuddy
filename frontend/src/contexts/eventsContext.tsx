f822eb2b50598ea67931abe997419aca2edef004
asallh <ajaypalsallh@gmail.com>
2024-03-12 09:45:12 -0600

    Creating the various events contexts
    Signed-off-by: asallh <ajaypalsallh@gmail.com>

--- frontend/src/contexts/eventsContext.tsx
+++ frontend/src/contexts/eventsContext.tsx
@@ -1,34 +1,52 @@
 import { useQuery } from "@tanstack/react-query";
 import type { PropsWithChildren } from "react";
-import { createContext } from "react";
-import { mockSearchEvents } from "~/mockData/EventData";
+import { createContext, useCallback } from "react";
+import { getHomePageEvents, getProfilePageEvents, getSearchPageEvents } from "~/lib/apiFunctions/Events";
+import { EventType } from "~/types/Events";
 
-type Event = {
-    id: string,
-    name: string
-    date: string,
-    location: string,
-    clubName: string,
-    picture: string
-}
 
 type eventsContext = {
-    events: Event[]
+  homePageEvents: EventType[],
+  searchPageEvents: EventType[],
+  profilePageEvents: EventType[]
 };
 const EventsContext = createContext<eventsContext | null>(null);
 
-export const EventsContextProvider = ({ children }: PropsWithChildren): JSX.Element => {
-    const { data: events } = useQuery({
-        queryKey: ['events'],
-        queryFn: async () => mockSearchEvents,
-        initialData: []
-    })
+export const EventsContextProvider = ({
+  children,
+}: PropsWithChildren): JSX.Element => {
+  const { data: homePageEvents } = useQuery({
+    queryKey: ["home-page-events"],
+    queryFn: getHomePageEvents,
+    initialData: [],
+  });
 
-    return (
-        <EventsContext.Provider value={{ events }}>
-            {children}
-        </EventsContext.Provider>
-    );
+  const { data: searchPageEvents } = useQuery({
+    queryKey: ["search-page-events"],
+    queryFn: getSearchPageEvents,
+    initialData: [],
+  });
+
+  const { data: profilePageEvents } = useQuery({
+    queryKey: ["profile-page-events"],
+    queryFn: getProfilePageEvents,
+    initialData: [],
+  });
+
+//   type handleEventPressArg = {
+//     type: 'home' | 'search' | 'profile',
+//     id: string
+//   }
+//   const handleEventPress = ({ type, id }: handleEventPressArg) => {
+//     const events = type === 'home' ? homePageEvents : type === 'search' ? searchPageEvents : profilePageEvents
+//     const event = events.find(event => event.id === id) 
+//   }
+
+  return (
+    <EventsContext.Provider value={{ homePageEvents, searchPageEvents, profilePageEvents }}>
+      {children}
+    </EventsContext.Provider>
+  );
 };
 
 export default EventsContext;
