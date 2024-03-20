1f3fbb9635abb59a9f866814dcbfb867253d290c
AarshShah9 <aarshshah99@gmail.com>
2024-03-20 16:28:33 -0600

    Animation working

--- frontend/src/navigation/LandingGroup/BottomTabsGroup/SearchGroup/Header.tsx
+++ frontend/src/navigation/LandingGroup/BottomTabsGroup/SearchGroup/Header.tsx
@@ -1,106 +1,171 @@
-import { View, StyleSheet, TouchableOpacity, TouchableWithoutFeedback } from "react-native";
+import {
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import useThemeContext from "~/hooks/useThemeContext";
-import { useCallback, useState } from "react";
+import {
+  Dispatch,
+  SetStateAction,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import useAppContext from "~/hooks/useAppContext";
-import { ThemedTextInput } from "~/components/ThemedComponents";
+import { ThemedText, ThemedTextInput } from "~/components/ThemedComponents";
+import { Animated } from "react-native";
 
-const SearchArea = () => {
-    const { theme } = useThemeContext();
-    
-    const [filterWord, setFilterWord] = useState("");
-    const updateFilterWord = useCallback((text: string) => {
-        setFilterWord(text)
-    }, [])
-    const clearSearchArea = useCallback(() => {
-        setFilterWord("")
-    }, [])  
+const SearchArea = ({
+  setIsFocused,
+  animateWidth,
+}: {
+  setIsFocused: Dispatch<SetStateAction<boolean>>;
+  animateWidth: Animated.AnimatedInterpolation<number>;
+}) => {
+  const { theme } = useThemeContext();
 
-    const { dismissKeyboard } = useAppContext();
+  const [filterWord, setFilterWord] = useState("");
+  const updateFilterWord = useCallback((text: string) => {
+    setFilterWord(text);
+  }, []);
+  const clearSearchArea = useCallback(() => {
+    setFilterWord("");
+  }, []);
 
-    return (
-        <View
-            style={[
-                styles.searchBar,
-                { backgroundColor: `${theme.colors.background}` },
-            ]}
-        >
-            <TouchableOpacity onPress={dismissKeyboard}>
-                <AntDesign name="search1" size={20} color={theme.colors.text} />
-            </TouchableOpacity>
-            <ThemedTextInput
-                placeholder="Search"
-                placeholderTextColor={theme.colors.text}
-                style={[styles.searchBarInput, { color: theme.colors.text }]}
-                value={filterWord}
-                onChangeText={updateFilterWord}
-            />
-            {filterWord !== "" && (
-                <TouchableOpacity onPress={clearSearchArea}>
-                <AntDesign
-                    name="closecircle"
-                    size={15}
-                    color={theme.colors.text}
-                />
-                </TouchableOpacity>
-            )}
-        </View>
-    )
+  const { dismissKeyboard } = useAppContext();
+
+  const onFocus = () => setIsFocused(true);
+  const onBlur = () => setIsFocused(false);
+
+  return (
+    <Animated.View
+      style={[
+        styles.searchBar,
+        { width: animateWidth, backgroundColor: `${theme.colors.background}` },
+      ]}
+    >
+      {/*<TouchableOpacity onPress={dismissKeyboard}>*/}
+      <TouchableOpacity>
+        <AntDesign name="search1" size={20} color={theme.colors.text} />
+      </TouchableOpacity>
+      <ThemedTextInput
+        placeholder="Search"
+        placeholderTextColor={theme.colors.text}
+        style={[styles.searchBarInput, { color: theme.colors.text }]}
+        value={filterWord}
+        onChangeText={updateFilterWord}
+        onFocus={onFocus}
+        onBlur={onBlur}
+      />
+      {filterWord !== "" && (
+        <TouchableOpacity onPress={clearSearchArea}>
+          <AntDesign name="closecircle" size={15} color={theme.colors.text} />
+        </TouchableOpacity>
+      )}
+    </Animated.View>
+  );
 };
 
 export default function Header() {
-    const insets = useSafeAreaInsets();
-    const { theme } = useThemeContext();
-    const { dismissKeyboard } = useAppContext();
+  const insets = useSafeAreaInsets();
+  const { theme } = useThemeContext();
+  const { dismissKeyboard } = useAppContext();
+  const [isFocused, setIsFocused] = useState(false);
+  const animateWidth = useRef(new Animated.Value(100)).current;
 
-    return (
-        <TouchableWithoutFeedback onPress={dismissKeyboard}>
-            <View 
-                style={[
-                    styles.headerContainer, 
-                    { 
-                        backgroundColor: theme.colors.primary, 
-                        paddingTop: insets.top + 15,
-                        paddingBottom: 15
-                    }
-                ]}
+  const triggerAnimation = (focus: boolean) => {
+    const duration = focus ? 150 : 300; // Faster when focused, slower when blurred
+    Animated.timing(animateWidth, {
+      toValue: focus ? 75 : 100,
+      duration,
+      useNativeDriver: false,
+    }).start();
+  };
+
+  useEffect(() => {
+    triggerAnimation(isFocused);
+  }, [isFocused]);
+
+  return (
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+        setIsFocused(false);
+      }}
+    >
+      <View
+        style={[
+          styles.headerContainer,
+          {
+            backgroundColor: theme.colors.primary,
+            paddingTop: insets.top,
+            paddingBottom: 10,
+          },
+        ]}
+      >
+        <View style={styles.searchArea}>
+          <SearchArea
+            setIsFocused={setIsFocused}
+            animateWidth={animateWidth.interpolate({
+              inputRange: [75, 100],
+              outputRange: ["75%", "100%"],
+            })}
+          />
+          {isFocused && (
+            <TouchableOpacity
+              style={styles.cancelButton}
+              onPress={() => {
+                Keyboard.dismiss();
+                setIsFocused(false);
+              }}
             >
-                <View style={styles.searchArea}>
-                    <SearchArea />
-                    <AntDesign name="filter" size={30} color={theme.colors.onSecondary} />
-                </View>
-            </View>
-        </TouchableWithoutFeedback>
-    );
+              <ThemedText style={{ color: theme.colors.text }}>
+                Cancel
+              </ThemedText>
+            </TouchableOpacity>
+          )}
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  );
 }
 
-
 const styles = StyleSheet.create({
-    headerContainer: {
-        alignItems: "center",
-        paddingHorizontal: 20,
-        paddingBottom: 0,
-    },
-    searchArea: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        width: "100%",
-    },
-    searchBar: {
-        flexDirection: "row",
-        alignItems: "center",
-        width: "87%",
-        minWidth: 300,
-        minHeight: 34,
-        borderRadius: 10,
-        paddingLeft: 6,
-    },
-    searchBarInput: {
-        height: "90%",
-        marginHorizontal: 5,
-        fontSize: 18,
-        flex: 0.98,
-    }
+  headerContainer: {
+    alignItems: "center",
+    paddingHorizontal: 20,
+    paddingBottom: 0,
+  },
+  searchArea: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+  },
+  searchBar: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+    minWidth: 300,
+    minHeight: 34,
+    borderRadius: 10,
+    paddingLeft: 6,
+  },
+  searchBarInput: {
+    height: "90%",
+    marginHorizontal: 5,
+    fontSize: 18,
+    flex: 0.98,
+  },
+  focusedSearchBar: {
+    flex: 1,
+  },
+  cancelButton: {
+    padding: 10,
+  },
 });
--- frontend/src/navigation/LandingGroup/BottomTabsGroup/SearchGroup/SearchTabs.tsx
+++ frontend/src/navigation/LandingGroup/BottomTabsGroup/SearchGroup/SearchTabs.tsx
@@ -6,68 +6,66 @@ import Marketplace from "~/screens/Marketplace";
 import Services from "~/screens/Services";
 
 type BarIconProps = {
-    focused: boolean,
-    color: string
-}
+  focused: boolean;
+  color: string;
+};
 
 const EventsBarIcon = ({ color, focused }: BarIconProps) => {
-    return (
-        <MaterialCommunityIcons
-            name={focused ? "calendar-blank" : "calendar-blank-outline"}
-            size={22}
-            color={color}
-        /> 
-    )
-}
+  return (
+    <MaterialCommunityIcons
+      name={focused ? "calendar-blank" : "calendar-blank-outline"}
+      size={22}
+      color={color}
+    />
+  );
+};
 
 const ServicesBarIcon = ({ color }: BarIconProps) => {
-    return (
-        <FontAwesome name={"binoculars"} size={22} color={color} />
-    )
-}
+  return <FontAwesome name={"binoculars"} size={22} color={color} />;
+};
 
 const MarketplaceBarIcon = ({ color, focused }: BarIconProps) => {
-    return (
-        <MaterialCommunityIcons
-            name={focused ? "shopping" : "shopping-outline"}
-            size={22}
-            color={color}
-        />
-    )
-}
+  return (
+    <MaterialCommunityIcons
+      name={focused ? "shopping" : "shopping-outline"}
+      size={22}
+      color={color}
+    />
+  );
+};
 
 const TopTabs = createMaterialTopTabNavigator();
 
 export default function SearchTabs() {
-    const { theme } = useThemeContext();
+  const { theme } = useThemeContext();
 
-    return (
-        <TopTabs.Navigator
-            screenOptions={{
-                tabBarStyle: { 
-                    backgroundColor: theme.colors.background,
-                    shadowColor: 'grey',
-                    shadowOffset: { width: 0, height: 3 },
-                    shadowOpacity: 0.4,
-                    shadowRadius: 3
-                }
-            }}
-        >
-            <TopTabs.Screen
-                name="Events"
-                component={Events}
-                options={{ tabBarIcon: EventsBarIcon }}
-            />
-            <TopTabs.Screen
-                name="Looking For"
-                component={Services}
-                options={{ tabBarIcon: ServicesBarIcon }}
-            />
-            <TopTabs.Screen
-                name="Market"
-                component={Marketplace}
-                options={{ tabBarIcon: MarketplaceBarIcon }}
-            />
-        </TopTabs.Navigator>
-    )
+  return (
+    <TopTabs.Navigator
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: theme.colors.background,
+          shadowColor: "grey",
+          shadowOffset: { width: 0, height: 3 },
+          shadowOpacity: 0.4,
+          shadowRadius: 3,
+        },
+      }}
+    >
+      <TopTabs.Screen
+        name="Events"
+        component={Events}
+        options={{ tabBarIcon: EventsBarIcon, tabBarLabel: () => null }}
+      />
+      <TopTabs.Screen
+        name="Looking For"
+        component={Services}
+        options={{ tabBarIcon: ServicesBarIcon, tabBarLabel: () => null }}
+      />
+      <TopTabs.Screen
+        name="Market"
+        component={Marketplace}
+        options={{ tabBarIcon: MarketplaceBarIcon, tabBarLabel: () => null }}
+      />
+    </TopTabs.Navigator>
+  );
 }
