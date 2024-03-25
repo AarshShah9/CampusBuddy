34bb9ea023a4ae18e42137882c64160604ce8604
AarshShah9 <aarshshah99@gmail.com>
2024-03-24 21:38:57 -0600

    removed un-n comments
    Signed-off-by: AarshShah9 <aarshshah99@gmail.com>

--- frontend/src/screens/Home.tsx
+++ frontend/src/screens/Home.tsx
@@ -1,4 +1,10 @@
-import { RefreshControl, View, Image, Dimensions, TouchableOpacity } from "react-native";
+import {
+  RefreshControl,
+  View,
+  Image,
+  Dimensions,
+  TouchableOpacity,
+} from "react-native";
 import { FlashList } from "@shopify/flash-list";
 import HorizontalScrollElement from "~/components/HorizontalScrollElement";
 import Carousel from "pinar";
@@ -13,114 +19,124 @@ import { getMainEvents } from "~/lib/apiFunctions/Events";
 import { EventData, EventType } from "~/types/Events";
 import LoadingSkeleton from "~/components/LoadingSkeleton";
 
-const fetchMainEvents = async (): Promise<{ allEvents: EventData[], startingEvents: EventType[] }> => {
-    try {
-        const { data: { allEvents, startingEvents } } = await getMainEvents()
-        return { allEvents, startingEvents }
-    } catch (error) {
-        console.error('An error occured while fetching main events:\n', error)
-        return { allEvents: [], startingEvents: [] }
-    }
-}
+const fetchMainEvents = async (): Promise<{
+  allEvents: EventData[];
+  startingEvents: EventType[];
+}> => {
+  try {
+    const {
+      data: { allEvents, startingEvents },
+    } = await getMainEvents();
+    return { allEvents, startingEvents };
+  } catch (error) {
+    console.error("An error occured while fetching main events:\n", error);
+    return { allEvents: [], startingEvents: [] };
+  }
+};
 
 export default function Home() {
-    const { expoPushToken, sendNotification } = usePushNotifications();
+  const { expoPushToken, sendNotification } = usePushNotifications();
 
-    useEffect(() => {
-        sendNotification({
-            title: '🎉Welcome to Campus Buddy🎉',
-            body: `Your journey to a better campus experience just began!`
-        })
-        .catch(error => console.log('An error occured when trying to send a notification:\n', error))
-    }, [expoPushToken])
+  useEffect(() => {
+    sendNotification({
+      title: "🎉Welcome to Campus Buddy🎉",
+      body: `Your journey to a better campus experience just began!`,
+    }).catch((error) =>
+      console.log(
+        "An error occured when trying to send a notification:\n",
+        error,
+      ),
+    );
+  }, [expoPushToken]);
 
-    const screenWidth = Dimensions.get("window").width;
-    const { startLoading, stopLoading } = useLoadingContext();
+  const screenWidth = Dimensions.get("window").width;
+  const { startLoading, stopLoading } = useLoadingContext();
 
-    const { navigateTo } = useNavigationContext();
+  const { navigateTo } = useNavigationContext();
 
-    const openEventDetails = useCallback((id: string) => {
-        navigateTo({ page: "EventDetails", id });
-    }, []);
+  const openEventDetails = useCallback((id: string) => {
+    navigateTo({ page: "EventDetails", id });
+  }, []);
 
-    const { refreshing, triggerRefresh, stopRefresh } = useRefreshControl();
+  const { refreshing, triggerRefresh, stopRefresh } = useRefreshControl();
 
-    const onPullRefresh = useCallback(() => {
-        triggerRefresh(() => {
-            refetch()
-        })
-    }, [])
+  const onPullRefresh = useCallback(() => {
+    triggerRefresh(() => {
+      refetch();
+    });
+  }, []);
 
-    const { data, isLoading, refetch, isFetchedAfterMount, isFetching } = useQuery({
-        queryKey: ["home-page-events"],
-        queryFn: fetchMainEvents,
+  const { data, isLoading, refetch, isFetchedAfterMount, isFetching } =
+    useQuery({
+      queryKey: ["home-page-events"],
+      queryFn: fetchMainEvents,
     });
 
-    useEffect(() => {
-        if(isLoading)
-            startLoading()
-        else
-            stopLoading()
-    }, [isLoading])
+  useEffect(() => {
+    if (isLoading) startLoading();
+    else stopLoading();
+  }, [isLoading]);
 
-    const queryIsLoading = isFetching && isFetchedAfterMount
-    useEffect(() => {
-        if(!queryIsLoading)
-            stopRefresh()
-    }, [queryIsLoading])
+  const queryIsLoading = isFetching && isFetchedAfterMount;
+  useEffect(() => {
+    if (!queryIsLoading) stopRefresh();
+  }, [queryIsLoading]);
 
-    /**
-     * the data is {"allEvents": [{"id": "1", "items": [Array], "title": "Attending"}, {"id": "2", "items": [Array], "title": "Upcoming Events From Following"}, {"id": "3", "items": [Array], "title": "Trending Events"}, {"id": "4", "items": [Array], "title": "Happening Today"}, {"id": "5", "items": [Array], "title": "Explore Verified Organizations"}], "startingEvents": [{"id": "79bc7200-c551-11ee-83fd-6f8d6c450910", "image": "https://d2epenzoyf672m.cloudfront.net/pfp/calgaryExpo.jpg", "title": "Career Fair"}]}
-     */
-    
-    const allEvents = data ? data.allEvents : []
-    const startingEvents = data ? data.startingEvents : []
+  const allEvents = data ? data.allEvents : [];
+  const startingEvents = data ? data.startingEvents : [];
 
-    return (
-        <View style={{ flex: 1 }}>
-            <FlashList
-                refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onPullRefresh} />}
-                data={allEvents}
-                estimatedItemSize={20}
-                extraData={queryIsLoading}
-                renderItem={({ item }) => <HorizontalScrollElement item={item} isLoading={queryIsLoading} />}
-                keyExtractor={item => item.id}
-                ListHeaderComponent={
-                    <View
-                        style={{
-                            justifyContent: "center",
-                            marginBottom: 32,
-                            alignItems: "center",
-                        }}
-                    >
-                        <Carousel
-                            loop={true}
-                            autoplay={true}
-                            autoplayInterval={5000}
-                            showsControls={false}
-                            style={{
-                                width: screenWidth,
-                                height: 214,
-                            }}
-                        >
-                            {startingEvents.map(item => (
-                                <LoadingSkeleton 
-                                    key={item.image} 
-                                    show={queryIsLoading} 
-                                    width={screenWidth} height={214} radius='square'>
-                                    <TouchableOpacity onPress={() => openEventDetails(item.id)}>
-                                        <Image
-                                            key={item.image}
-                                            source={{ uri: generateImageURL(item.image) }}
-                                            style={{ width: screenWidth, height: 214 }}
-                                        />
-                                    </TouchableOpacity>
-                                </LoadingSkeleton>
-                            ))}
-                        </Carousel>
-                    </View>
-                }
-            />
-        </View>
-    );
+  return (
+    <View style={{ flex: 1 }}>
+      <FlashList
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onPullRefresh} />
+        }
+        data={allEvents}
+        estimatedItemSize={20}
+        extraData={queryIsLoading}
+        renderItem={({ item }) => (
+          <HorizontalScrollElement item={item} isLoading={queryIsLoading} />
+        )}
+        keyExtractor={(item) => item.id}
+        ListHeaderComponent={
+          <View
+            style={{
+              justifyContent: "center",
+              marginBottom: 32,
+              alignItems: "center",
+            }}
+          >
+            <Carousel
+              loop={true}
+              autoplay={true}
+              autoplayInterval={5000}
+              showsControls={false}
+              style={{
+                width: screenWidth,
+                height: 214,
+              }}
+            >
+              {startingEvents.map((item) => (
+                <LoadingSkeleton
+                  key={item.image}
+                  show={queryIsLoading}
+                  width={screenWidth}
+                  height={214}
+                  radius="square"
+                >
+                  <TouchableOpacity onPress={() => openEventDetails(item.id)}>
+                    <Image
+                      key={item.image}
+                      source={{ uri: generateImageURL(item.image) }}
+                      style={{ width: screenWidth, height: 214 }}
+                    />
+                  </TouchableOpacity>
+                </LoadingSkeleton>
+              ))}
+            </Carousel>
+          </View>
+        }
+      />
+    </View>
+  );
 }
