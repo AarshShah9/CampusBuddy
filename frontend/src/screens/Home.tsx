3c932f9a9c76d5864afd2cef0100197c810efbd2
AarshShah9 <aarshshah99@gmail.com>
2024-03-24 22:20:34 -0600

    Formatting
    Signed-off-by: AarshShah9 <aarshshah99@gmail.com>

--- frontend/src/screens/Home.tsx
+++ frontend/src/screens/Home.tsx
@@ -37,18 +37,18 @@ const fetchMainEvents = async (): Promise<{
 export default function Home() {
   const { expoPushToken, sendLocalNotification } = usePushNotifications();
 
-    useEffect(() => {
-        if (!expoPushToken) return;
-        sendLocalNotification({
-            title: "🎉Welcome to Campus Buddy🎉",
-            body: `Your journey to a better campus experience just began!`,
-        }).catch((error) =>
-            console.log(
-                "An error occured when trying to send a notification:\n",
-                error,
-            ),
-        );
-    }, [expoPushToken]);
+  useEffect(() => {
+    if (!expoPushToken) return;
+    sendLocalNotification({
+      title: "🎉Welcome to Campus Buddy🎉",
+      body: `Your journey to a better campus experience just began!`,
+    }).catch((error) =>
+      console.log(
+        "An error occured when trying to send a notification:\n",
+        error,
+      ),
+    );
+  }, [expoPushToken]);
 
   const screenWidth = Dimensions.get("window").width;
   const { startLoading, stopLoading } = useLoadingContext();
