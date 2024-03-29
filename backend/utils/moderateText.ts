72ae8cf2d35e2eb693a6b6a0bf206d4cb81c1665
nomanSanjari <nomansanjari2001@gmail.com>
2024-03-29 14:27:33 -0600

    Implemented filtering for custom bad words
    Signed-off-by: nomanSanjari <nomansanjari2001@gmail.com>

--- backend/utils/moderateText.ts
+++ backend/utils/moderateText.ts
@@ -1,5 +1,7 @@
 import OpenAI from "openai";
 import { env } from "./validateEnv";
+import Filter from "bad-words";
+import { string } from "zod";
 
 const openai = new OpenAI({
   apiKey: env.GPT_KEY,
@@ -10,18 +12,32 @@ export const moderateText = async (
   title: string,
   description: string | null,
 ) => {
+  // add customBadWords here as necessary
+  const customBadWords: string[] = ["fuck", "fucking", "fucker"];
+
+  // custom filter using bad-words cuz we need to moderate fuck and all deritavitves smh
+  const filter = new Filter();
+  filter.addWords(...customBadWords);
+
   // because JS/TS scoping is weird
   let descriptionFlagged;
+  let isProfaneDescription;
 
   const responseTitle = await openai.moderations.create({ input: title });
   const titleFlagged = responseTitle.results[0].flagged;
+
+  const isProfaneTitle = filter.isProfane(title);
 
   if (description !== null) {
     const responseDescription = await openai.moderations.create({
       input: description,
     });
     descriptionFlagged = responseDescription.results[0].flagged;
+
+    isProfaneDescription = filter.isProfane(description);
   }
 
-  return titleFlagged || descriptionFlagged;
+  return (
+    titleFlagged || descriptionFlagged || isProfaneTitle || isProfaneDescription
+  );
 };
