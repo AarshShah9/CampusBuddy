3fd373e788703278fd3de1b671db52664d93d5e3
AarshShah9 <aarshshah99@gmail.com>
2024-03-28 22:46:57 -0600

    Removed un imports

--- frontend/src/contexts/eventsContext.tsx
+++ frontend/src/contexts/eventsContext.tsx
@@ -1,29 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
 import { createContext, PropsWithChildren } from "react";
-import { ImagePickerAsset } from "expo-image-picker";
-import {
-  attendEvent,
-  createEvent,
-  createMarketPlaceItem,
-  createPost,
-  getAllMapEvents,
-  getAllPosts,
-  getAttendees,
-  getEventDetails,
-  getMarketPlaceItems,
-  getProfilePageEvents,
-  getSearchPageEvents,
-  likeEvent,
-} from "~/lib/apiFunctions/Events";
-import {
-  AttendeeResponse,
-  EventDetailsProps,
-  EventType,
-  lookingForDetail,
-  MarketPlaceItem,
-  SearchPageEventType,
-} from "~/types/Events";
-import { createEventType } from "~/screens/CreateEvent";
 
 type eventsContext = {};
 const EventsContext = createContext<eventsContext | null>(null);
