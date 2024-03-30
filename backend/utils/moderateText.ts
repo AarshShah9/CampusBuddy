8c1231cbd33d56469df63445745c1d1fee506b8b
AarshShah9 <aarshshah99@gmail.com>
2024-03-29 18:15:12 -0600

    Added try catch
    Signed-off-by: AarshShah9 <aarshshah99@gmail.com>

--- backend/utils/moderateText.ts
+++ backend/utils/moderateText.ts
@@ -1,7 +1,6 @@
 import OpenAI from "openai";
 import { env } from "./validateEnv";
 import Filter from "bad-words";
-import { string } from "zod";
 
 const openai = new OpenAI({
   apiKey: env.GPT_KEY,
@@ -12,32 +11,47 @@ export const moderateText = async (
   title: string,
   description: string | null,
 ) => {
-  // add customBadWords here as necessary
-  const customBadWords: string[] = ["fuck", "fucking", "fucker"];
+  try {
+    // add customBadWords here as necessary
+    const customBadWords: string[] = [
+      "fuck",
+      "fucking",
+      "fucker",
+      "shit",
+      "bitch",
+      "cunt",
+    ];
 
-  // custom filter using bad-words cuz we need to moderate fuck and all deritavitves smh
-  const filter = new Filter();
-  filter.addWords(...customBadWords);
+    const filter = new Filter();
+    filter.addWords(...customBadWords);
 
-  // because JS/TS scoping is weird
-  let descriptionFlagged;
-  let isProfaneDescription;
+    let descriptionFlagged;
+    let isProfaneDescription;
 
-  const responseTitle = await openai.moderations.create({ input: title });
-  const titleFlagged = responseTitle.results[0].flagged;
+    const responseTitle = await openai.moderations.create({ input: title });
+    const titleFlagged = responseTitle.results[0].flagged;
 
-  const isProfaneTitle = filter.isProfane(title);
+    const isProfaneTitle = filter.isProfane(title);
 
-  if (description !== null) {
-    const responseDescription = await openai.moderations.create({
-      input: description,
-    });
-    descriptionFlagged = responseDescription.results[0].flagged;
+    console.log("title", title, isProfaneTitle);
 
-    isProfaneDescription = filter.isProfane(description);
+    if (description !== null) {
+      const responseDescription = await openai.moderations.create({
+        input: description,
+      });
+      descriptionFlagged = responseDescription.results[0].flagged;
+
+      isProfaneDescription = filter.isProfane(description);
+    }
+
+    return (
+      titleFlagged ||
+      descriptionFlagged ||
+      isProfaneTitle ||
+      isProfaneDescription
+    );
+  } catch (error) {
+    console.log(error);
+    return false;
   }
-
-  return (
-    titleFlagged || descriptionFlagged || isProfaneTitle || isProfaneDescription
-  );
 };
