fc981c4595b9ee25e78d5668fd9a9da206548b6f
JamesRobert20 <jmtendamema@gmail.com>
2024-04-02 16:54:56 -0600

    cleaned oorganization posts
    Signed-off-by: JamesRobert20 <jmtendamema@gmail.com>

--- frontend/src/screens/OrganizationPosts.tsx
+++ frontend/src/screens/OrganizationPosts.tsx
@@ -1,6 +1,6 @@
 import { Pressable, RefreshControl, StyleSheet, View } from "react-native";
-import { ThemedText } from "~/components/ThemedComponents";
-import { useNavigationState, useRoute } from "@react-navigation/native";
+import { Text } from "react-native-paper";
+import { useRoute } from "@react-navigation/native";
 import { useQuery } from "@tanstack/react-query";
 import { getUserProfilePosts } from "~/lib/apiFunctions/Profile";
 import useLoadingContext from "~/hooks/useLoadingContext";
@@ -11,115 +11,105 @@ import LookingForItem from "~/components/SearchLookingForBar";
 import { PostType } from "~/types/LookingFor";
 
 export default function OrganizationPosts() {
-  const {
-    params: { id },
-  } = useRoute<any>();
+    const { params: { id } } = useRoute<any>();
 
-  // TODO figure out a better way to determine if it's your posts
-  const isYour = useNavigationState((state) => {
-    const route = state.routes[state.index];
-    return route.name;
-  }).includes("Your");
-
-  const {
-    data: userProfilePosts,
-    isLoading,
-    refetch,
-    isFetchedAfterMount,
-    isFetching,
-  } = useQuery<PostType[]>({
-    queryKey: ["user-posts", id],
-    queryFn: () => getUserProfilePosts(id),
-    initialData: [],
-  });
-
-  const { startLoading, stopLoading } = useLoadingContext();
-
-  const { refreshing, triggerRefresh, stopRefresh } = useRefreshControl();
-
-  const onPullRefresh = useCallback(() => {
-    triggerRefresh(() => {
-      refetch();
+    const {
+        data: organizationPosts,
+        isLoading,
+        refetch,
+        isFetchedAfterMount,
+        isFetching,
+    } = useQuery<PostType[]>({
+        queryKey: ["organization-posts"],
+        queryFn: () => getUserProfilePosts(id),
+        initialData: [],
     });
-  }, []);
 
-  useEffect(() => {
-    if (isLoading) startLoading();
-    else stopLoading();
-  }, [isLoading]);
+    const { startLoading, stopLoading } = useLoadingContext();
 
-  const queryIsLoading = isFetching && isFetchedAfterMount;
-  useEffect(() => {
-    if (!queryIsLoading) stopRefresh();
-  }, [queryIsLoading]);
+    const { refreshing, triggerRefresh, stopRefresh } = useRefreshControl();
 
-  return (
-    <View style={{ flex: 1 }}>
-      <FlashList
-        data={userProfilePosts}
-        showsVerticalScrollIndicator={false}
-        renderItem={({ item }) => (
-          <Pressable>
-            <View style={{ paddingHorizontal: 20 }}>
-              <LookingForItem
-                key={item.id}
-                title={item.title}
-                description={item.description}
-                requiredMembers={item.spotsLeft}
-                id={item.id}
-              />
-            </View>
-          </Pressable>
-        )}
-        keyExtractor={(item) => item.id}
-        contentContainerStyle={{ paddingTop: 20 }}
-        estimatedItemSize={20}
-        refreshControl={
-          <RefreshControl refreshing={refreshing} onRefresh={onPullRefresh} />
-        }
-        extraData={queryIsLoading}
-        ListHeaderComponent={() => (
-          <ThemedText
-            style={{
-              paddingLeft: 20,
-              fontFamily: "Nunito-Bold",
-              fontSize: 24,
-            }}
-          >
-            {isYour ? "Your Posts" : "Posts"}
-          </ThemedText>
-        )}
-        ListEmptyComponent={() => {
-          return (
-            <>
-              {!isLoading && !isFetching && (
-                <View
-                  style={{
-                    flex: 1,
-                    justifyContent: "center",
-                    alignItems: "center",
-                  }}
-                >
-                  <ThemedText style={{ textAlign: "center", marginTop: 150 }}>
-                    {isYour
-                      ? "You haven't made any posts yet!"
-                      : "No posts found."}
-                  </ThemedText>
-                </View>
-              )}
-            </>
-          );
-        }}
-      />
-    </View>
-  );
+    const onPullRefresh = useCallback(() => {
+        triggerRefresh(() => {
+            refetch();
+        });
+    }, []);
+
+    useEffect(() => {
+        if (isLoading) startLoading();
+        else stopLoading();
+    }, [isLoading]);
+
+    const queryIsLoading = isFetching && isFetchedAfterMount;
+    useEffect(() => {
+        if (!queryIsLoading) stopRefresh();
+    }, [queryIsLoading]);
+
+    return (
+        <View style={{ flex: 1 }}>
+            <FlashList
+                data={organizationPosts}
+                showsVerticalScrollIndicator={false}
+                renderItem={({ item }) => (
+                    <Pressable>
+                        <View style={{ paddingHorizontal: 20 }}>
+                            <LookingForItem
+                                key={item.id}
+                                title={item.title}
+                                description={item.description}
+                                requiredMembers={item.spotsLeft}
+                                id={item.id}
+                            />
+                        </View>
+                    </Pressable>
+                )}
+                keyExtractor={(item) => item.id}
+                contentContainerStyle={{ paddingTop: 20 }}
+                estimatedItemSize={20}
+                refreshControl={
+                    <RefreshControl refreshing={refreshing} onRefresh={onPullRefresh} />
+                }
+                extraData={queryIsLoading}
+                ListHeaderComponent={() => (
+                    <Text
+                        style={{
+                            paddingLeft: 20,
+                            fontFamily: "Nunito-Bold",
+                            fontSize: 24,
+                        }}
+                    >
+                        Posts
+                    </Text>
+                )}
+                ListEmptyComponent={() => {
+                    return (
+                        <>
+                            {!isLoading && !isFetching && (
+                                <View
+                                    style={{
+                                        flex: 1,
+                                        justifyContent: "center",
+                                        alignItems: "center",
+                                    }}
+                                >
+                                    <Text style={{ textAlign: "center", marginTop: 150 }}>
+                                        No posts found.
+                                    </Text>
+                                </View>
+                            )}
+                        </>
+                    );
+                }}
+            />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  itemsContainer: {
-    flexDirection: "row",
-    justifyContent: "center",
-    flexWrap: "wrap",
-    paddingHorizontal: 20,
-  },
+    itemsContainer: {
+        flexDirection: "row",
+        justifyContent: "center",
+        flexWrap: "wrap",
+        paddingHorizontal: 20,
+    },
 });
