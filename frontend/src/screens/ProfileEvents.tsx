e202203a1eabfa1ec93aae9d8ebace498cc39fc2
JamesRobert20 <jmtendamema@gmail.com>
2024-04-02 13:17:03 -0600

    fixed render item
    Signed-off-by: JamesRobert20 <jmtendamema@gmail.com>

--- frontend/src/screens/ProfileEvents.tsx
+++ frontend/src/screens/ProfileEvents.tsx
@@ -57,7 +57,7 @@ export default function ProfileEvents() {
       <FlatList
         data={events as any}
         showsVerticalScrollIndicator={false}
-        renderItem={HorizontalScrollElement}
+        renderItem={({ item }) => <HorizontalScrollElement key={item.id} item={item} isLoading={false} />}
         keyExtractor={(item) => item.id}
         contentContainerStyle={{ paddingTop: 20 }}
       />
