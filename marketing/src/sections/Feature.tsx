1c9215ed6f674c3970de18e6314b606ebc6e9300
Johnny Tran <jktran1998@gmail.com>
2024-05-27 19:05:20 -0600

    website responsiveness to 1024x width and up.
    Signed-off-by: Johnny Tran <jktran1998@gmail.com>

--- marketing/src/sections/Feature.tsx
+++ marketing/src/sections/Feature.tsx
@@ -1,32 +1,42 @@
 export default function Feature(){
     return (
-        <div style={{width:"100%"}}>
+        <div>
             <div className="mt-8">
-                <div style={{backgroundColor:"#F7F9FF", width:"100%"}} className="flex-row flex p-14">
+                <div style={{backgroundColor:"#F7F9FF", width:"100%"}} className="lg:flex-row flex p-14">
                     <div style={{width:"100%",justifyContent:"center", display:"flex"}}>
                         <img style={{height:"700px"}} src="\Features\EventsFeature.png"/>
                     </div>
-                    <div className="mx-4">
-                        <h2 className="text-3xl  font-bold tracking-tight text-gray-900 ">Personalized Campus Event at Your Fingertips</h2>
-                        <p className="text-2xl leading-8 text-gray-600 pt-4">Discover events happening on and around campus right from the homepage. Our unique algorithm recommends events tailored to your interests.</p>
+                    <div className="mx-4 w-11/12">
+                        <h2 className="lg:text-3xl  font-bold tracking-tight text-gray-900  ">Personalized Campus Event at Your Fingertips</h2>
+                        <p className="lg:text-2xl  leading-8 text-gray-600 pt-4 ">Discover events happening on and around campus right from the homepage. Our unique algorithm recommends events tailored to your interests.</p>
                     </div>
                 </div>
-                <div className="flex-row flex p-14">
-                    <div className="mx-4">
-                        <h2 className="text-3xl font-bold tracking-tight text-gray-900 ">Looking for</h2>
-                        <p className="text-2xl leading-8 text-gray-600 pt-4">Our unique thread system caters to all student needs. Whether you’re looking for an extra player for pickleball or need a carpool, post your request here and get help from your peers!</p>
+
+                <div style={{width:"100%"}} className="lg:flex-row flex p-14">
+                    <div className="mx-4 w-11/12 ">
+                        <h2  className="lg:text-3xl  font-bold tracking-tight text-gray-900">Looking For Something? </h2>
+                        <p className="lg:text-2xl  leading-8 text-gray-600 pt-4 ">Are you itching to find a partner for a quick pick-up game or perhaps share your latest meme? Our “Looking For” feature is here to connect students seeking anything they need. Whether it's finding a study buddy, a ride share, or someone who loves the same hobbies, post your request and get connected with your peers instantly.</p>
                     </div>
                     <div style={{width:"100%",justifyContent:"center", display:"flex"}}>
                         <img  style={{height:"700px"}} src="\Features\LookingForFeature.png"/>
                     </div>
                 </div>
-                <div style={{backgroundColor:"#F7F9FF"}} className="flex-row flex p-14">
+                <div style={{backgroundColor:"#F7F9FF"}} className="lg:flex-row flex p-14">
                     <div style={{width:"100%",justifyContent:"center", display:"flex"}}>
                         <img  style={{height:"700px"}} src="\Features\ProfilePage.png"/>
                     </div>
-                    <div className="mx-4">
-                        <h2 className="text-3xl font-bold tracking-tight text-gray-900 ">Customize your own profile</h2>
-                        <p className="text-2xl leading-8 text-gray-600 pt-4">Keep track of all the events that you have attended or upcoming to share with peers, and check events that other users are attending to find events to attend with your peers!</p>
+                    <div className="mx-4 w-11/12">
+                        <h2  className="lg:text-3xl  font-bold tracking-tight text-gray-900   ">Customize your own profile</h2>
+                        <p className="xl:text-2xl  leading-8 text-gray-600 pt-4 ">Keep track of all the events that you have attended or upcoming to share with peers, and check events that other users are attending to find events to attend with your peers!</p>
+                    </div>
+                </div>
+                <div className="lg:flex-row flex p-14">
+                    <div className="mx-4 w-11/12 ">
+                        <h2  className="lg:text-3xl  font-bold tracking-tight text-gray-900   ">Unlock Insights with Our Analytics</h2>
+                        <p className="lg:text-2xl  leading-8 text-gray-600 pt-4 ">Our app's analytics feature provides comprehensive statistics and tailored posts, empowering organizations to connect with their target market more effectively. Gain valuable insights, track engagement, and optimize your outreach strategies with our user-friendly reporting tools.</p>
+                    </div>
+                    <div style={{width:"100%",justifyContent:"center", display:"flex"}}>
+                        <img  style={{height:"700px"}} src="\Features\analytic.png"/>
                     </div>
                 </div>
             </div>
--- marketing/src/sections/UVPSection.tsx
+++ marketing/src/sections/UVPSection.tsx
@@ -2,10 +2,10 @@ export default function UVPSection() {
     return(
         
             <div className="text-center p-32">
-                <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+                <h1 className=" font-bold tracking-tight text-gray-900 sm:text-5xl">
                     Made by Students, for Students
                 </h1>
-                <p className="mt-6 text-2xl leading-8 text-gray-600 px-56 ">Campus Buddy was founded on the belief that students deserve a richer, more connected university experience, recognizing the untapped potential for vibrant campus communities and empowering student engagement.</p>
+                <p className="mt-6 sm:text-2xl leading-8 text-gray-600  xl:px-56 ">Campus Buddy was founded on the belief that students deserve a richer, more connected university experience, recognizing the untapped potential for vibrant campus communities and empowering student engagement.</p>
             </div>
      
     )
